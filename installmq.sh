ae26339f0b01d54f7c3065229f4dadd7bb48b231
Marcel <66844942+Bakri-10@users.noreply.github.com>
2024-02-11 19:07:00 +0100

    Update installmq.sh

--- installmq.sh
+++ installmq.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-# Path to the bash script you want to copy
-BASH_SCRIPT_PATH="/path/to/update-dockefile.sh"
+# Define the path to search in. Replace /path/to/directory with your actual directory path.
+SEARCH_PATH="/path/to/directory"
 
-# Base directory to start searching for Dockerfiles
-SEARCH_BASE_DIR="/path/to/search/directory"
-
-# Find directories containing a Dockerfile and copy the bash script into each
-find "$SEARCH_BASE_DIR" -type f -name 'Dockerfile' | while read dockerfile; do
-  DIR_PATH=$(dirname "$dockerfile")
-  echo "Copying $BASH_SCRIPT_PATH to $DIR_PATH"
-  cp "$BASH_SCRIPT_PATH" "$DIR_PATH"
-done
+# Search for Dockerfile in the specified path and echo each path found
+find "$SEARCH_PATH" -type f -name "Dockerfile" -exec echo {} \;
